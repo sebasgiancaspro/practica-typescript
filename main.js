9a95065c32b9c3f3d2db3e6e9d5f9ec80a6b3e78
Sebastian <sebasgiancaspro@gmail.com>
2021-06-01 13:22:22 -0300

    Se realizo en el main el ejemplo de los ANY

--- main.js
+++ main.js
@@ -43,3 +43,19 @@ console.log(linternaVerde);
 console.log(volcanNegro);
 var concat = "Los heroes son: " + batman + ", " + linternaVerde + " y " + volcanNegro;
 console.log(concat);
+/*
+Ejemplos de Tipos de datos Any(quiere decir que una misma variable puede servir para cualquier cosa)
+Es decir como un string, boolean, number etc.
+Eso se ve con el ejemplo de la variable "vengador".
+*/
+var vengdor;
+var existe;
+var derrotas;
+vengdor = "Spider-man";
+console.log(vengdor.charAt(1)); /*te imprime en este caso la segunda letra "p" xq empieza del 0*/
+vengdor = 150.555555;
+console.log(vengdor.toFixed(2)); /*te imprime dos numeros mas despues del punto*/
+vengdor = true;
+console.log(vengdor); /*te imprime vengador xq es true*/
+console.log(existe); /*te imprime valor "undefine" o indefinido x ser ANY*/
+console.log(derrotas); /*te imprime valor "undefine" o indefinido x ser ANY*/
--- main.ts
+++ main.ts
@@ -55,3 +55,27 @@ console.log(volcanNegro);
 let concat:string = `Los heroes son: ${batman}, ${linternaVerde} y ${volcanNegro}`
 
 console.log(concat);
+
+
+/*
+Ejemplos de Tipos de datos Any(quiere decir que una misma variable puede servir para cualquier cosa)
+Es decir como un string, boolean, number etc.
+Eso se ve con el ejemplo de la variable "vengador".
+*/
+
+let vengdor;
+let existe;
+let derrotas;
+
+vengdor= "Spider-man";
+console.log(vengdor.charAt(1)); /*te imprime en este caso la segunda letra "p" xq empieza del 0*/
+
+vengdor= 150.555555;
+console.log(vengdor.toFixed(2)); /*te imprime dos numeros mas despues del punto*/
+
+vengdor= true;
+console.log(vengdor); /*te imprime vengador xq es true*/
+
+console.log(existe); /*te imprime valor "undefine" o indefinido x ser ANY*/
+
+console.log(derrotas); /*te imprime valor "undefine" o indefinido x ser ANY*/
